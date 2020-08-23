8bf09f994191ab5a392b7f43ea13397c1e79b811
Kevin <kburo21@gmail.com>
2020-08-23 14:41:38 -0500

    Add toolbar up arrow. Add basic example of navigating from one frag to another using fab.

---
+++ Dogs/app/src/main/java/com/jetpack/dogs/view/ListFragment.kt
@@ -0,0 +1,32 @@
+package com.jetpack.dogs.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.jetpack.dogs.R
+import kotlinx.android.synthetic.main.fragment_list.*
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_details.setOnClickListener {
+            val actionLaunchDetailFragment = ListFragmentDirections.actionListFragmentToDetailFragment()
+            actionLaunchDetailFragment.dogUuid = 5
+            Navigation.findNavController(it).navigate(actionLaunchDetailFragment)
+        }
+    }
+
+}
